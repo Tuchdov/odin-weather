6ee5686f2c8b32ae17c83654a11819ca93d1ee7a
Tuchdov <tuchdov@gmail.com>
2026-01-12 09:43:16 +0200

    Initial commit

---
+++ src/dom.js
@@ -0,0 +1,8 @@
+// You’re going to want functions that can take a location
+//  and return the weather data for that location.
+//  For now, just console.log() the information.
+
+export async function getWeatherData(location) {
+    
+    
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './style.css'
+
+import { setupCounter } from './weather.js'
+
