3999ea8b877e8912915fa7e8f7a7be95c2652836
Tuchdov <tuchdov@gmail.com>
2026-01-27 20:14:58 +0200

    Add main.js logic

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import './style.css'
 
-import { setupCounter } from './weather.js'
+
+import * as dom from './dom.js' 
+import * as weather  from './weather.js' 
+
+// So first I put the get weather data inside the init
+// but how do I do the rendering 
+// because I need to render functions inside and not one?
+
+dom.initLocationForm( async (location) => {
+    try {
+    dom.clearError()
+    const  current = await weather.getCurrentData(location);
+    const   forecast = await weather.getForecast(location);
+
+    dom.renderCurrentWeather(current);
+    dom.renderForecast(forecast);
+    }
+    catch(error){
+       dom.showError(error.message)
+    }
+})
 
