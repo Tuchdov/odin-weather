e016111a1706cc5ab4955eb762586309f17d324b
Tuchdov <tuchdov@gmail.com>
2026-02-03 10:03:03 +0200

    Move api to a .env file

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,3 @@
-"use strict";
 /**
  * Gets data from visual crossing
  * @param {string} location - The location to get the weather from
@@ -9,7 +8,11 @@ export async function getWeatherData(location) {
     throw new Error("No location provided");
   }
 
-  const API_KEY = "F7DAMFZQK663JES2VYHE3SHKU"; // this is public and free to use
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+  if (!API_KEY) {
+    throw new Error('Weather API key is not configured');
+  }
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`,
   );
@@ -43,16 +46,58 @@ export async function getCurrentData(location) {
   };
 }
 
+
+
+
+// Refactor weather.js to have extraction functions
+export function extractCurrentData(weatherData) {
+  const { resolvedAddress } = weatherData;
+  const { temp, feelslike, conditions, humidity, uvindex, windspeed, icon } =
+    weatherData.currentConditions;
+  
+  return {
+    resolvedAddress,
+    temp,
+    feelslike,
+    conditions,
+    humidity,
+    uvindex,
+    windspeed,
+    icon,
+  };
+}
+
 /**
  * Gets weather for next 3 days
  * @param {string} location - The location to get the weather from
  * @returns {Array}] with The relevant fields, each day is an object
  */
 
+
 export async function getForecast(location) {
   const weatherData = await getWeatherData(location);
   const weatherArr = [];
   for (let i = 1; i <= 3; i++) {
+    // create an object and push it to the array
+    const forecast =
+      (({ datetime, tempmax, tempmin, sunrise, sunset, conditions, icon }) => ({
+        datetime,
+        tempmax,
+        tempmin,
+        sunrise,
+        sunset,
+        conditions,
+        icon,
+      }))(weatherData.days[i]);
+    weatherArr.push(forecast);
+  }
+  return weatherArr;
+}
+
+export function extractForecastData(weatherData) {
+  const FORECAST_DAYS = 3
+  const weatherArr = [];
+  for (let i = 1; i <= FORECAST_DAYS; i++) {
     // create an object and push it to the array
     const forecast =
       (({ datetime, tempmax, tempmin, sunrise, sunset, conditions, icon }) => ({
